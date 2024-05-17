d44221e2e306effca20021fbe410f34daeab1460
choiminkyung <choialsrud2002@gmail.com>
2024-05-17 14:37:05 +0900

    파일 추가 : ExampleViewController

---
+++ SeSacFirstApp0514/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  SeSacFirstApp0514
+//
+//  Created by 최민경 on 5/17/24.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("333")
+    }
+}
